b624d297b56069aec0a933bffb59fcba89729d70
bobwatson_century <gaurav@acmeminds.com>
2022-01-27 16:30:47 +0530

    history added

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -35,67 +35,14 @@ export class HistoryComponent implements OnInit {
     });
   }
 
+  
 
-  addClick(){
-    this.dep={
-      DepartmentId:0,
-      DepartmentName:""
-    }
-    this.ModalTitle="Add Department";
-    this.ActivateAddEditDepComp=true;
+ 
 
-  }
+ 
 
-  editClick(item){
-    this.dep=item;
-    this.ModalTitle="Edit Department";
-    this.ActivateAddEditDepComp=true;
-  }
+ 
 
-  deleteClick(item){
-    if(confirm('Are you sure??')){
-      this.service.deleteDepartment(item.DepartmentId).subscribe(data=>{
-        alert(data.toString());
-        this.refreshDepList();
-      })
-    }
-  }
-
-  closeClick(){
-    this.ActivateAddEditDepComp=false;
-    this.refreshDepList();
-  }
-
-
-  refreshDepList(){
-    this.service.getDepList().subscribe(data=>{
-      this.DepartmentList=data;
-      this.DepartmentListWithoutFilter=data;
-    });
-  }
-
-  FilterFn(){
-    var DepartmentIdFilter = this.DepartmentIdFilter;
-    var DepartmentNameFilter = this.DepartmentNameFilter;
-
-    this.DepartmentList = this.DepartmentListWithoutFilter.filter(function (el){
-        return el.DepartmentId.toString().toLowerCase().includes(
-          DepartmentIdFilter.toString().trim().toLowerCase()
-        )&&
-        el.DepartmentName.toString().toLowerCase().includes(
-          DepartmentNameFilter.toString().trim().toLowerCase()
-        )
-    });
-  }
-
-  sortResult(prop,asc){
-    this.DepartmentList = this.DepartmentListWithoutFilter.sort(function(a,b){
-      if(asc){
-          return (a[prop]>b[prop])?1 : ((a[prop]<b[prop]) ?-1 :0);
-      }else{
-        return (b[prop]>a[prop])?1 : ((b[prop]<a[prop]) ?-1 :0);
-      }
-    })
-  }
+  
 
 }
